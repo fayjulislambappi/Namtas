505a954e85e9420ff9b228a9800e532e272237bb
Md. Fayjul Islam Bappi <77269490+fayjulislambappi@users.noreply.github.com>
2023-12-05 22:26:15 +0600

    Create 5namtas.js

---
+++ 5namtas.js
@@ -0,0 +1,12 @@
+function printNamtas6(range) {
+      for (var i = 1; i <= range; i++) {
+        var result = 6 * i;
+        console.log("6 x " + i + " = " + result);
+      }
+    }
+
+    // Change the range as needed
+    var tableRange = 10;
+
+    // Print Namtas 6 up to the specified range
+    printNamtas6(tableRange);
