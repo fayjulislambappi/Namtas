d46e73a1c7955b7aa881e34e69c579a7cf810cb3
Md. Fayjul Islam Bappi <77269490+fayjulislambappi@users.noreply.github.com>
2023-12-05 22:22:39 +0600

    Create 3namtas.js

---
+++ 3namtas.js
@@ -0,0 +1,10 @@
+// Function to print the N times table for 3
+function printNamta3(N) {
+  for (let i = 1; i <= N; i++) {
+    let result = 3 * i;
+    console.log(`3 x ${i} = ${result}`);
+  }
+}
+
+// Print the N times table for 3, where N is 10 (you can change N to any desired number)
+printNamta3(10);
